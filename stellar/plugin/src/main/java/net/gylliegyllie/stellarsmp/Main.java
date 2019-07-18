e9885a37335c470c5767ea13d79a6752461f9176
GyllieGyllie <GyllieGyllie@gmail.com>
2019-07-18 19:22:15 +0200

    [StellarSMP] remove discord code

--- stellar/plugin/src/main/java/net/gylliegyllie/stellarsmp/Main.java
+++ stellar/plugin/src/main/java/net/gylliegyllie/stellarsmp/Main.java
@@ -1,22 +1,11 @@
 package net.gylliegyllie.stellarsmp;
 
-import net.gylliegyllie.stellarsmp.discord.DiscordManager;
-import net.gylliegyllie.stellarsmp.listeners.ChatListener;
 import net.gylliegyllie.stellarsmp.listeners.JoinListener;
-import net.gylliegyllie.stellarsmp.listeners.LeaveListener;
 import net.gylliegyllie.stellarsmp.threads.LiveThread;
 import net.gylliegyllie.stellarsmp.utils.ThreadUtil;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
-	private DiscordManager discordManager;
-
-	public Main() {
-		//this.discordManager = new DiscordManager(this);
-	}
 
 	@Override
 	public void onEnable() {
@@ -25,12 +14,6 @@ public class Main extends JavaPlugin {
 
 		// Register listeners
 		this.getServer().getPluginManager().registerEvents(new JoinListener(this), this);
-		this.getServer().getPluginManager().registerEvents(new LeaveListener(this), this);
-		this.getServer().getPluginManager().registerEvents(new ChatListener(this), this);
-
-		// Register commands
-
-		if (this.discordManager != null) this.getDiscordManager().sendOnline();
 
 		new LiveThread(this);
 
@@ -39,16 +22,6 @@ public class Main extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
-		this.discordManager.shutdown();
-
-		for (Player player : Bukkit.getOnlinePlayers()) {
-			player.kickPlayer("Server is rebooting!");
-		}
-
 		this.getLogger().info("SMP Plugin disabled :(");
-	}
-
-	public DiscordManager getDiscordManager() {
-		return this.discordManager;
 	}
 }
